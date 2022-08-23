0bebf2cddabab3776787172ca26e66a40b0da9e1
Harrison Balogh <harrisonbalogh@gmail.com>
2022-08-23 15:27:14 -0700

    lib: stun server URL update

--- hx-seam-lib/Constants.js
+++ hx-seam-lib/Constants.js
@@ -3,5 +3,5 @@ export const AUTH_DOMAIN = 'https://www.harrisonbalogh.com'
 export const DOMAIN = 'wss://www.harrisonbalogh.com'
 export const RELAY_SERVER_URL = `${DOMAIN}/api/seam`
 
-export const STUN_SERVER_URL = 'stun:www.harrisonbalogh.com/api/stun'
+export const STUN_SERVER_URL = 'stun:harrisonbalogh.com'
 export const STUN_SERVER_ALT_URL = 'stun:stun.sipgate.net:19302'
