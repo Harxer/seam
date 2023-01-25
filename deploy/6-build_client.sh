301783f5199120a7bf196eb7b9176c5211b9a212
Harrison Balogh <harrisonbalogh@gmail.com>
2023-01-25 14:43:50 -0800

    copy directory deploy step update

--- deploy/6-build_client.sh
+++ deploy/6-build_client.sh
@@ -1,7 +1,7 @@
  #!/bin/bash
 
 cd ./client
-path=$HTTP_DIR/projects/$2
+path=$HTTP_DIR/../demo/$2
 mkdir -p $path
 # Rebuild client to include release changes
 (npm install; npm run build --production; cp -a ./build/. $path)
